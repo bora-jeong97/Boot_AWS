7fb45aa1a2d9aff9f03e556fd4a0057bf60ed1e3
bora <purple7714@naver.com>
2020-04-27 23:39:45 +0900

    BBS insert, update, select and localDate

---
+++ src/main/java/com/bora/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package com.bora.book.springboot.domain.posts;
+
+
+import com.bora.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity { //실제 db와 매칭될 클래스
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    //수정
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
